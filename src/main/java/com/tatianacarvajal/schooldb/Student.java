1b0f954b509b7122f46cdb272f6848e9bf4dd95b
Tatiana Carvajal <taticv159912@gmail.com>
2025-03-25 19:41:02 -0500

    Creating Student class and Student form

---
+++ src/main/java/com/tatianacarvajal/schooldb/Student.java
@@ -0,0 +1,38 @@
+package com.tatianacarvajal.schooldb;
+
+/**
+ *
+ * @author tatianacarvajal
+ */
+public class Student {
+    
+    int studentCode;
+    String studentName;
+    String studentSurname;
+
+    public int getStudentCode() {
+        return studentCode;
+    }
+
+    public void setStudentCode(int studentCode) {
+        this.studentCode = studentCode;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentSurname() {
+        return studentSurname;
+    }
+
+    public void setStudentSurname(String studentSurname) {
+        this.studentSurname = studentSurname;
+    }
+    
+      
+}
